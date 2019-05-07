c7e1766ebfeac301f6cef3c5cdeaff67d5a08817
Nikita <myepicland@gmail.com>
2019-05-08 00:15:55 +0200

    Initial commit

---
+++ DaftTap Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaftTap Challenge
+//
+//  Created by Nick on 08/05/2019.
+//  Copyright © 2019 Nikita Gulak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
